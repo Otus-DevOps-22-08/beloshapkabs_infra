820c79ecb4a445b8ff6350544f8957118f7ac736
beloshapka bogdan <beloshapkabs@gmail.com>
2022-09-22 22:12:14 +0300

    lesson 6 fix scripts

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install git
 
 git clone -b monolith https://github.com/express42/reddit.git
 
--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo apt update
+sudo apt-get install apt-transport-https ca-certificates git -y
+
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
