5f448eb9a2ceecded0332738ee86286a7176be56
beloshapka bogdan <beloshapkabs@gmail.com>
2022-09-26 01:08:21 +0300

    lesson 7 productive work with packer. Phew.

---
+++ packer/scripts/install_mongodb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt update
+apt-get install apt-transport-https ca-certificates git -y
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+apt-get update
+apt-get install -y mongodb-org
+
+systemctl start mongod
+systemctl enable mongod
