84eee341e984a3a444c0ad3b796d702bec0f01df
beloshapkabs <beloshapkabs@gmail.com>
2022-11-15 15:44:30 +0300

    lesson 13

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.80.0"
+    }
+  }
+}
+
 resource "yandex_compute_instance" "app" {
   name = "reddit-app"
 
